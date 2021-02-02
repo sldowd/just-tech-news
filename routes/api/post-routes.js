570c47f7983698acdb7e079cbb4e3579340a5e9b
Sarah Dowd <sarahlynnedowd@gmail.com>
2021-02-02 14:40:06 -0500

    added update and get single post routes

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -20,4 +20,79 @@ router.get('/', (req, res) => {
     });
 });
 
+// get single post
+router.get('/:id', (req,res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'post_url', 'title', 'created_at'],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+    
+router.post('/', (req,res) => {    
+    Post.create({
+        title: req.body.title,
+        post_url: req.body.post_url,
+        user_id: req.body.user_id
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// update post title
+router.put('/:id', (req, res) => {
+    Post.update(
+        {
+            title: req.body.title
+        },
+        {
+            where: {
+            id: req.params.id
+            }
+        }
+        )
+        .then(dbPostData => {
+            if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+  });
+
+// //get all posts from a user
+// router.get('/users/:userName', (req,res) => {
+//     User.findOne({
+//         where: {
+//             username: req.params.userName
+//         },
+//         include: [Post]
+//     })
+// })
+
 module.exports = router;
