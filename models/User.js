9190bd43775f7ee9a5bf0fee705267b72cce7c87
Sarah Dowd <sarahlynnedowd@gmail.com>
2021-02-02 12:39:34 -0500

    fixed bug in password validation on login route

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcrypt');
 class User extends Model {
     //set up method to run on instance data (per user) to check the password
     checkPassword(loginPw) {
-        return bcrypt.compareSync(loginPw. this.password);
+        return bcrypt.compareSync(loginPw, this.password);
     }
 }
 
