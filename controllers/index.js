295d1b0399bc299645948aae09f9c7f5e883c2f5
Sarah Dowd <sarahlynnedowd@gmail.com>
2021-02-06 09:15:10 -0500

    set up handlebars engine

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 
+const homeRoutes = require('./home-routes');
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
+// catch all route for 'route not found'
 router.use((req,res) => {
     res.status(404).end();
 });
