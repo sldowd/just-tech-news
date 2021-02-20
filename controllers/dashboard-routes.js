cd43076c2d2eed76c949cc657771ed3d9b3a46fa
Sarah Dowd <sarahlynnedowd@gmail.com>
2021-02-20 17:54:35 -0500

    coded dashboard handlebar

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comments } = require('../models');
+
+
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const homeRoutes = require('./home-routes');
 const apiRoutes = require('./api');
+const dashboardRoutes = require('./dashboard-routes');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 // catch all route for 'route not found'
 router.use((req,res) => {
