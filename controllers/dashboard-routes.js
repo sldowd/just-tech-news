c4dc9e78a2bc0d5d0c89d505a9906d545d1ff246
Sarah Dowd <sarahlynnedowd@gmail.com>
2021-02-20 19:03:14 -0500

    dashboard

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,6 +2,42 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comments } = require('../models');
 
-
+router.get('/', (req,res) => {
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comments,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        // serialize data before passing to template
+        const posts = dbPostData.map(post => post.get({ plain: true}));
+        res.render('dashboard', { loggedIn: true });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
